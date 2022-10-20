b9b8191126e127ad6f343bc186ecc2426fe8e6fc
Yogendra Rajpoot <yogendra@101innovations.in>
2022-10-20 11:17:56 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <p style={{ fontSize: "45px" }}>Yogendra Rajpoot</p>
+        <p style={{ fontSize: "20px", marginTop: "-2%" }}>Software Developer</p>
+        <p style={{ fontSize: "20px", marginTop: "-1%" }}>
+          Logic is everywhere.
         </p>
-        <a
+        {/* <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
       </header>
     </div>
   );
